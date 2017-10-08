043700f5ebaad6f9e241a567c8f7020ea4887fd8
Denny Scott <dennyscott301@gmail.com>
2017-10-08 00:03:43 -0500

    Socket is passed through oauth, and user details are connected to socket

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,44 @@
+import { getUserFromAuthToken } from './domain/users';
+import * as authModel from './domain/auth';
+import OAuth2Server from'oauth2-server';
+const Request = OAuth2Server.Request;
+const Response = OAuth2Server.Response;
+
 const init = (io) => {
-  console.log('initalizing sockets');
-  io.on('connection', client => {
-    client.emit('test', 'other data');
+  io.use((socket, next) => {
+    let token = socket.handshake.query.token;
+    const oauth = new OAuth2Server({
+      model: authModel,
+      requireClientAuthentication: { password: false}
+    });
+
+    let request = new Request({
+      method: 'GET',
+      query: {},
+      headers: {Authorization: `Bearer ${token}`}
+    });
+
+    let response = new Response({
+      headers: {},
+    });
+
+    oauth.authenticate(request, response)
+      .then(token => {
+        const {firstname, lastname, email, username, _id} = token.user;
+        socket.user = {
+          firstname,
+          lastname,
+          email,
+          username,
+          _id
+        };
+        return next();
+      })
+      .catch(err => next(new Error("Socket was not authenticated")));
+  });
+
+  io.on('connection', socket => {
+    socket.emit('test', 'other data');
   })
 }
 
